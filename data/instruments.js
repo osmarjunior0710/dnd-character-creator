fb745f6acfbba02d61b2dc6cbe6c9ce51b49e406
osmarjunior0710 <osmar.melo.junior@gmail.com>
2026-08-05 09:07:23 -0300

    Add files via upload

---
+++ data/instruments.js
@@ -0,0 +1,5 @@
+/* PENDÊNCIA RESOLVIDA: essa é agora a fonte oficial da lista de
+   instrumentos (10 do capítulo 6 de Equipamento, incluindo a grafia
+   "Flauta de Pã"). Compartilhado com o Bardo (data/classes/bardo.js) e
+   qualquer antecedente/classe futura que precise da mesma lista. */
+const ALL_INSTRUMENTS = ["Alaúde","Flauta","Flauta de Pã","Gaita de Foles","Lira","Oboé","Tambor","Trombeta","Violino","Xilofone"];
